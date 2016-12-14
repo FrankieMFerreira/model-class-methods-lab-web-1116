df1fb129c36fc32f4a1300f4c4fb5760fca12098
FrankieMFerreira <FrankieMFerreira@gmail.com>
2016-12-14 16:58:48 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,31 @@
+require 'pry'
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.select('captain.name').includes(:boats => :classifications)
+   .where({classifications: {name:  'Catamaran'}})
+  end
+
+  def self.sailors
+    self.select('captain.name').includes(:boats => :classifications)
+   .where({classifications: {name:  'Sailboat'}}).uniq
+  end
+
+  def self.motorboats
+    self.select('captain.name').includes(:boats => :classifications)
+   .where({classifications: {name:  'Motorboat'}}).uniq
+ end
+
+  def self.talented_seamen
+    k = self.sailors.pluck(:id) & self.motorboats.pluck(:id)
+    self.where(:id => k)
+  end
+
+  def self.non_sailors
+    sailors = self.sailors.pluck(:id)
+    self.where.not(:id => sailors)
+  end
+
+
 end
